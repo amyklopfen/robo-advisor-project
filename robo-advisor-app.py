c4fcd1a487c55e1cc809860c79fe3fd6276f6b37
amyklopfen <50849294+amyklopfen@users.noreply.github.com>
2019-06-22 12:39:43 -0400

    updated with latest day and datetime now code

--- robo-advisor-app.py
+++ robo-advisor-app.py
@@ -19,31 +19,29 @@ response = requests.get(requests_url)
 
 parsed_response = json.loads(response.text)
 
-breakpoint()
+last_refreshed = parsed_response["Meta Data"]["3. Last Refreshed"]
+#breakpoint()
 
-quit()
+
 
 load_dotenv() #> loads contents of the .env file into the script's environment
 
 API_KEY = os.environ.get("ALPHAVANTAGE_API_KEY") # default to using the "demo" key if an Env Var is not supplied
 
-while investing: 
-
-    try: 
-        company_stock = input("Please input the stock symbol for a company of your choice: ")
-        
-        if len(company_stock) <= 5:
-            break 
-        else:
-            print("Error, please enter a valid stock symbol") 
-    except ValueError:
-        print("Error, please enter a valid stock")
     
-def get_response(company_stock):
-    request_url = f"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={company_stock}&apikey={API_KEY}"
-    response = requests.get(request_url)
-    parsed_response = json.loads(response.text)
-    return parsed_response
-
-
+print("------------------------")
+print("SELECTED SYMBOL: MSFT")
+print("------------------------")
+print("REQUESTING STOCK MARKET DATA...")
+print("REQUEST AT: ", datetime.datetime.now())
+print("------------------------")
+print(f"LATEST DAY: {last_refreshed}")
+print("RECENT HIGH: $11111")
+print("RECENT LOW: $999")
+print("-----------------------")
+print("RECOMMENDATION: BUY!")
+print("BECAUSE: TODO")
+print("------------------------")
+print("HAPPY INVESTING!")
+print("-----------------------")
 
